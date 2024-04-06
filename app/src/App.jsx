a82decdd9a3fd35dd4d0e9405da9c5b5155d3ba7
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-04-06 16:20:46 +0200

    rebuild project, app and server initialized from the scratch

---
+++ app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="app">
+        <h1>Front</h1>      
+      </div>
+    </>
+  )
+}
+
+export default App
--- server/src/allItems.js
+++
@@ -1,8 +0,0 @@
-const connection = require ('./connection')
-
-const allItems = async () => {
-    const [query] = await connection.execute('SELECT * FROM tabla');
-    return query;
-}
-
-module.exports = allItems;
