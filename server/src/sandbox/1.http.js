4d1b67742092311cfe54651abbdbf920f9e8fa28
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-04-12 22:08:01 +0200

    routes wirking on server

--- server/src/sandbox/1.http.js
+++ server/src/sandbox/1.http.js
@@ -3,13 +3,19 @@ const http = require("node:http");
 const desiredPort = process.env.PORT ?? 1234;
 
 const processRequest = (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+
   if (req.url === "/") {
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.end("Home Page");
+    res.end("<h1>Home Page</h1>");
+  } else if (req.url === "/contact") {
+    res.statusCode = 200;
+    res.end("<h1>Contact Page</h1>");
+  } else {
+    res.statusCode = 404;
+    res.end("<h1>404 Page Not Found</h1>");
   }
 };
-
 const server = http.createServer(processRequest);
 
 server.listen(desiredPort, () => {
