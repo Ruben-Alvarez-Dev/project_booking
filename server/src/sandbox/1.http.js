63bae2a7d816eeb380637aa78697f3e0e2260237
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-04-13 00:13:04 +0200

    res.end -> image with headers

--- server/src/sandbox/1.http.js
+++ server/src/sandbox/1.http.js
@@ -1,15 +1,25 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const desiredPort = process.env.PORT ?? 1234;
 
 const processRequest = (req, res) => {
   res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.statusCode = 200;
 
   if (req.url === "/") {
-    res.statusCode = 200;
     res.end("<h1>Home Page</h1>");
+  } else if (req.url === "/Photo.jpg") {
+    fs.readFile("./Photo.jpg", (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end("<h1>Internal Server Error</h1>");
+      } else {
+        res.setHeader("Content-Type", "image/jpg");
+        res.end(data);
+      }
+    });
   } else if (req.url === "/contact") {
-    res.statusCode = 200;
     res.end("<h1>Contact Page</h1>");
   } else {
     res.statusCode = 404;
