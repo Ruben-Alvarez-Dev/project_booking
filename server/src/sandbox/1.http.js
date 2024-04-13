f785b11883b346995f5fd8b812e999802ebe929b
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-04-13 14:37:14 +0200

    POST

--- server/src/sandbox/1.http.js
+++ server/src/sandbox/1.http.js
@@ -1,33 +1,33 @@
-const http = require("node:http");
-const fs = require("node:fs");
+const http = require('node:http')
+const fs = require('node:fs')
 
-const desiredPort = process.env.PORT ?? 1234;
+const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req, res) => {
-  res.setHeader("Content-Type", "text/html; charset=utf-8");
-  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  res.statusCode = 200
 
-  if (req.url === "/") {
-    res.end("<h1>Home Page</h1>");
-  } else if (req.url === "/Photo.jpg") {
-    fs.readFile("./Photo.jpg", (err, data) => {
+  if (req.url === '/') {
+    res.end('<h1>Home Page</h1>')
+  } else if (req.url === '/Photo.jpg') {
+    fs.readFile('./Photo.jpg', (err, data) => {
       if (err) {
-        res.statusCode = 500;
-        res.end("<h1>Internal Server Error</h1>");
+        res.statusCode = 500
+        res.end('<h1>Internal Server Error</h1>')
       } else {
-        res.setHeader("Content-Type", "image/jpg");
-        res.end(data);
+        res.setHeader('Content-Type', 'image/jpg')
+        res.end(data)
       }
-    });
-  } else if (req.url === "/contact") {
-    res.end("<h1>Contact Page</h1>");
+    })
+  } else if (req.url === '/contact') {
+    res.end('<h1>Contact Page</h1>')
   } else {
-    res.statusCode = 404;
-    res.end("<h1>404 Page Not Found</h1>");
+    res.statusCode = 404
+    res.end('<h1>404 Page Not Found</h1>')
   }
-};
-const server = http.createServer(processRequest);
+}
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
-  console.log(`Server is running on port: , ${desiredPort}`);
-});
+  console.log(`Server is running on port: , ${desiredPort}`)
+})
--- server/src/sandbox/2.routing.js
+++ server/src/sandbox/2.routing.js
@@ -1,25 +1,49 @@
-const http = require("node:http");
-const dittoJSON = require("./pokemon/ditto.json");
+const http = require('node:http');
+const dittoJSON = require('./pokemon/ditto.json');
 
 const processRequest = (req, res) => {
   const { method, url } = req;
 
   switch (method) {
-    case "GET":
+    case 'GET':
       switch (url) {
-        case "/pokemon/ditto":
-          res.setHeader("Content-Type", "application/json; charset=utf-8");
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8');
           return res.end(JSON.stringify(dittoJSON));
         default:
           res.statusCode = 404;
-          res.setHeader("Content-Type", "text/html; charset=utf-8");
-          return res.end("<h2>404</h2>");
+          res.setHeader('Content-Type', 'text/html; charset=utf-8');
+          return res.end('<h2>404</h2>');
+      }
+
+    case 'POST':
+      switch (url) {
+        case '/pokemon': {
+          let body = '';
+
+          req.on('data', chunk => {
+            body += chunk.toString();
+          });
+
+          req.on('end', () => {
+            const data = JSON.parse(body);
+            res.writeHead(201, {'Content-Type': 'application/json; charset=utf-8'});
+            res.end(JSON.stringify(data));
+          })
+          
+          break;
+        }
+
+        default:
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+          return res.end('404 Not Found');
       }
   }
-};
+}
 
 const server = http.createServer(processRequest);
 
 server.listen(1234, () => {
-  console.log("Server is running on port: 3002");
-});
+  console.log(`Server is running on port ${server.address().port}`);
+})
