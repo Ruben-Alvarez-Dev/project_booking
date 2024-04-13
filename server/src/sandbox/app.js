4ecb7483fef9158c85df6260f014d4e0c285d249
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-04-13 18:24:50 +0200

    POST working and sending new films to db

--- server/src/sandbox/app.js
+++ server/src/sandbox/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const crypto = require('node:crypto');
 const movies = require('./movies.json');
 
 // Create a new Express application
 const app = express();
 app.disable('x-powered-by');
 
+// Middleware
+app.use(express.json());
 
 // Define a route handler
 app.get('/', (req, res) => {
@@ -14,7 +17,7 @@ app.get('/movies', (req, res) => {
     const { genre } = req.query;
     if (genre) {
         const filteredMovies = movies.filter(
-            movie => movie.genre.includes(genre)
+            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
         )
         return res.json(filteredMovies);
     }
@@ -26,6 +29,31 @@ app.get('/movies/:id', (req, res) => {
     if (movie) return res.json(movie);
     res.status(404).json({ message: 'Movie not found' });
 });
+app.post('/movies', (req, res) => {
+    const {
+        title,
+        genre,
+        year,
+        director,
+        duration,
+        rate,
+        poster
+    } = req.body;
+    const newMovie = {
+        id: crypto.randomUUID(),
+        title,
+        genre,
+        director,
+        year,
+        duration,
+        rate: rate ?? 0,
+        poster
+    }
+    movies.push(newMovie);
+    res.status(201).json(newMovie);
+    }
+)
+
 
 // Start the server
 const PORT = process.env.PORT || 1234;
